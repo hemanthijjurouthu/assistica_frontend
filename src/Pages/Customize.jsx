dca3c548b953e10b07db2adcef109cac77d23b4e
hemanth_ijjurouthu <hemanth_ijjurouthu@hemanth-ijjurouthus-MacBook-Air.local>
2025-08-17 17:00:16 +0530

    add

--- src/Pages/Customize.jsx
+++ src/Pages/Customize.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useRef } from "react";
-import Card from "../components/card";
+import Card from "../components/Card";
 import image1 from "../assets/image1.png";
 import image2 from "../assets/image2.png";
 import image3 from "../assets/image3.png";
